fbdc111eb4bfd12ac63cb196bd41ed80c042057a
songyoungjun <clesaaaaaa@gmail.com>
2023-10-18 18:30:52 +0900

    2번째 페이지까지 스타일 완료

--- app.js
+++ app.js
@@ -35,16 +35,29 @@ http.createServer(function(req, res) {
                <form method="post" action="">
                  <div class='pp'><span class="cc">${data.id}</span>님! 반갑습니다. 저에게 편지를 보내주세요!</div>
                  <div>
-                  Title : <input type="text" name="tibox" placeholder=""></div>
+                  Title : <input type="text" id="tibox" placeholder=""></div>
                   <br>
-                  <div>Text : <input type="text" name="tbox"placeholder=""></div>
-                 <input class=btn type="submit" name="send">
+                  <div>Text : <input type="text" id="tbox" placeholder=""></div>
+                 <input class=btn type="submit" id="send">
                </form>
                <script>
                 let p = document.querySelector(".pp")
                 let c = document.querySelector(".cc")
+                let title = document.getElementById("tibox")
+                let text = document.getElementById("tbox")
+                let send = document.getElementById("send")
+                title.style.width="30vw"
+                title.style.height="30vh"
+                title.style.backgroundColor="${signUpAsset.inputBoxColor}"
+                text.style.width="30vw"
+                text.style.height="30vh"
+                text.style.backgroundColor="${signUpAsset.inputBoxColor}"
                 p.style.color="${signUpAsset.textColor}"
                 c.style.color="${signUpAsset.pointColor}"
+                send.style.width= "5vw"
+                send.style.height= "5vw"
+                send.style.color="white"
+                send.style.backgroundColor="${signUpAsset.pointColor}"
                </script>
              </body>
              </html>` )}
