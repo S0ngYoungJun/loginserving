f6cbf36c34370cff40cc59679b2f28b6461b2efd
songyoungjun <clesaaaaaa@gmail.com>
2023-10-18 18:20:54 +0900

    2번째 페이지 까지 해결

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var http = require('http');
 var fs = require("fs")
 var querystring = require('querystring');
+var signUpAsset = require('./module')
 const port = 3000;
 var regex=/^[a-z0-9]*$/;
 var regey=/^[A-Z0-9]*$/;
@@ -31,11 +32,19 @@ http.createServer(function(req, res) {
                <title>Document</title>
              </head>
              <body>
-               <form method="post" action="./login">
-                 <p>ID :${data.id}님! 반갑습니다. 저에게 편지를 보내주세요!</p>
-                 <input class=btn type="submit" onclick= "create_id();" name="Sign up">
+               <form method="post" action="">
+                 <div class='pp'><span class="cc">${data.id}</span>님! 반갑습니다. 저에게 편지를 보내주세요!</div>
+                 <div>
+                  Title : <input type="text" name="tibox" placeholder=""></div>
+                  <br>
+                  <div>Text : <input type="text" name="tbox"placeholder=""></div>
+                 <input class=btn type="submit" name="send">
                </form>
                <script>
+                let p = document.querySelector(".pp")
+                let c = document.querySelector(".cc")
+                p.style.color="${signUpAsset.textColor}"
+                c.style.color="${signUpAsset.pointColor}"
                </script>
              </body>
              </html>` )}
